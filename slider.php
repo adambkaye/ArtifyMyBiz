a3e071fbecaeab1e3009abc94d1ad174fe052a01
adambkaye <adam@railroadsquare.com>
2015-03-13 18:53:14 -1000

    Update slider.php

--- slider.php
+++ slider.php
@@ -1,12 +1,13 @@
    <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1>Artists & Business<br> Owners helping <br>each other!</h1>
-        <p><a class="btn btn-primary btn-lg howitworks" role="button">How It Works</a></p>
+         <!-- <h1>Artists & Business<br> Owners helping <br>each other!</h1> -->
+         <h1>Businesses get art! Artists improve their Business.</h1>
+         <p><a class="btn btn-primary btn-lg howitworks" role="button">How It Works</a></p>
       </div>
     </div>
     <div class="container">
-   		<h3 class="homePitch">Artists want to display their art to the public and businesses want nicely designed walls without having to pay much (if any).  Sounds like the perfect match!</h3><br>
+   	<h3 class="homePitch">Artists want to display their art to the public and businesses want nicely designed walls without having to pay much (if any).  Sounds like the perfect match!</h3><br>
     	<div class="line"></div>
  	</div>
     
